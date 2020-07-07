fcc0541a811429ddfd38c14ed566d4c6d6364ef0
Kristi <kltakach@gmail.com>
2020-07-07 06:59:53 -0400

    got sticky nav working, but styling still needs work. updated "interested in" section.

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-resume',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
 
+  @ViewChild('stickyMenu') menuElement: ElementRef
+
+  sticky: boolean = false
+  elementPosition: any
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this.elementPosition = this.menuElement.nativeElement.offsetTop
+
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  handleScroll(){
+    const windowScroll = window.pageYOffset
+    if(windowScroll >= this.elementPosition){
+      this.sticky = true
+    } else {
+      this.sticky = false
+    }
   }
 
 }
